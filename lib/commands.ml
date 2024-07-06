15e18c1122d4ef66cd24165bbfb04f0545284687
Dario Hett <dariohett+github@gmail.com>
2024-07-06 18:27:17 +0200

    Initial commit

---
+++ lib/commands.ml
@@ -0,0 +1,9 @@
+open Ppx_yojson_conv_lib.Yojson_conv.Primitives
+
+type command = 
+  | CreateInventoryItem of string * string
+  | DeactivateInventoryItem of string * int
+  | RenameInventoryItem of string * int * string
+  | CheckInItemsToInventory of string * int * int
+  | RemoveItemsFromInventory of string * int * int
+  [@@deriving yojson]
