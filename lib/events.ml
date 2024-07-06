48e32db9fe1e1e5f578a8eef3431b48c618c41bb
Dario Hett <dariohett+github@gmail.com>
2024-07-06 18:32:36 +0200

    events

---
+++ lib/events.ml
@@ -0,0 +1,27 @@
+open Ppx_yojson_conv_lib.Yojson_conv.Primitives
+
+type event =
+  | InventoryItemCreated of string * string
+  | InventoryItemDeactivated of string
+  | InventoryItemRenamed of string * string
+  | ItemsCheckedInToInventory of string * int
+  | ItemsRemovedFromInventory of string * int
+[@@deriving yojson]
+
+let id = function
+  | InventoryItemCreated (id, _) -> id
+  | InventoryItemDeactivated id -> id
+  | InventoryItemRenamed (id, _) -> id
+  | ItemsCheckedInToInventory (id, _) -> id
+  | ItemsRemovedFromInventory (id, _) -> id
+;;
+
+let to_string = function
+  | InventoryItemCreated (i, n) -> "Item " ^ n ^ " created (id:" ^ i ^ ")"
+  | InventoryItemDeactivated i -> "Item deactivated (id:" ^ i ^ ")"
+  | InventoryItemRenamed (i, n) -> "Item renamed to " ^ n ^ " created (id:" ^ i ^ ")"
+  | ItemsCheckedInToInventory (i, c) ->
+    "Check-in " ^ Int.to_string c ^ " of item (id:" ^ i ^ ")"
+  | ItemsRemovedFromInventory (i, c) ->
+    "Removed " ^ Int.to_string c ^ " of item (id:" ^ i ^ ")"
+;;
